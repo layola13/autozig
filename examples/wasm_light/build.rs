9cf585ea0fc9509d497ef39908b00dd15a42545b
layola <layola13@gmail.com>
2026-01-09 23:54:41 +0800

    fixed build target

--- examples/wasm_light/build.rs
+++ examples/wasm_light/build.rs
@@ -1,4 +1,14 @@
 fn main() -> anyhow::Result<()> {
+    // Check for WASM target
+    let target = std::env::var("TARGET").unwrap_or_default();
+    if !target.contains("wasm") {
+        println!(
+            "cargo:warning=Skipping compilation of autozig-wasm-light for non-WASM target: {}",
+            target
+        );
+        return Ok(());
+    }
+
     autozig_build::build("src")?;
     Ok(())
 }
